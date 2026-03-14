9a58a377809319814c2fceb2e3990a554b1974d8
techankit09 <ankitrawat0988@gmail.com>
2026-03-14 19:02:21 +0530

    ok

--- ORSProject-10-UI/src/app/app-routing.module.ts
+++ ORSProject-10-UI/src/app/app-routing.module.ts
@@ -1,7 +1,55 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { RoleComponent } from './role/role.component';
+import { UserComponent } from './user/user.component';
+import { CollegeComponent } from './college/college.component';
+import { StudentComponent } from './student/student.component';
+import { MarksheetComponent } from './marksheet/marksheet.component';
+import { CourseComponent } from './course/course.component';
+import { SubjectComponent } from './subject/subject.component';
+import { TimetableComponent } from './timetable/timetable.component';
+import { FacultyComponent } from './faculty/faculty.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+
+   {
+    path: 'role',
+    component: RoleComponent
+  },
+  {
+    path: 'user',
+    component: UserComponent
+  },
+  {
+    path: 'college',
+    component: CollegeComponent
+  },
+  {
+    path: 'student',
+    component: StudentComponent
+  },
+  {
+    path: 'marksheet',
+    component: MarksheetComponent
+  },
+  {
+    path: 'course',
+    component: CourseComponent
+  },
+  {
+    path: 'subject',
+    component: SubjectComponent
+  },
+  {
+    path: 'timetable',
+    component: TimetableComponent
+  },
+  {
+    path: 'faculty',
+    component: FacultyComponent
+  }
+
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- ORSProject-10-UI/src/app/app.module.ts
+++ ORSProject-10-UI/src/app/app.module.ts
@@ -1,21 +1,49 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
+import { RouterModule } from '@angular/router';
 
 import { HttpClientModule } from '@angular/common/http';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { HttpServiceService } from './http-service.service';
+import { NavbarComponent } from './navbar/navbar.component';
+import { RoleComponent } from './role/role.component';
+import { UserComponent } from './user/user.component';
+import { CollegeComponent } from './college/college.component';
+import { StudentComponent } from './student/student.component';
+import { MarksheetComponent } from './marksheet/marksheet.component';
+import { CourseComponent } from './course/course.component';
+import { SubjectComponent } from './subject/subject.component';
+import { TimetableComponent } from './timetable/timetable.component';
+import { FacultyComponent } from './faculty/faculty.component';
+import { FormsModule } from '@angular/forms';
+import { DashboardComponent } from './dashboard/dashboard.component';
+import { FooterComponent } from './footer/footer.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    NavbarComponent,
+    RoleComponent,
+    UserComponent,
+    CollegeComponent,
+    StudentComponent,
+    MarksheetComponent,
+    CourseComponent,
+    SubjectComponent,
+    TimetableComponent,
+    FacultyComponent,
+    DashboardComponent,
+    FooterComponent
   ],
   imports: [
     BrowserModule,
     AppRoutingModule,
-    HttpClientModule
+    HttpClientModule,
+    FormsModule
   ],
   providers: [HttpServiceService],
-  bootstrap: [AppComponent]
+  bootstrap: [AppComponent],
+
 })
 export class AppModule { }
